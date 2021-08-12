1d6e282671cfabb5cb6c22a3caaca88f447f6be0
nhatuk2710 <49880246+nhatuk2710@users.noreply.github.com>
2021-08-12 20:35:21 +0700

    first

---
+++ src/test/java/com/example/minhhang/MinhhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minhhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhhangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
